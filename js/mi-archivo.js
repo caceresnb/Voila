0569b78c330e7ee2eeff6b96fc93595da8aa09a8
caceresnb <84637134+caceresnb@users.noreply.github.com>
2021-10-26 15:46:31 -0300

    Add files via upload

---
+++ js/mi-archivo.js
@@ -0,0 +1,9 @@
+let nombre = prompt ("Hola! Cuál es tu nombre?")
+let saludo = "Bienvenid@ a Voilá " + nombre + "" + "!"
+console.log (nombre)
+alert(saludo)
+let mayoria = prompt ("Por favor dinos tu año de nacimiento")
+let calculoEdad= parseInt(2021 - mayoria)
+let respuesta = nombre +  " tienes " + calculoEdad
+console.log (calculoEdad)
+alert(respuesta)
